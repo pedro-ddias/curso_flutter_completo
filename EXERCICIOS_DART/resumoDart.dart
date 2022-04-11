1260f2bd66ada78c0704957407062c5f4f44610a
pedro-ddias <pedro.hdias@outlook.com>
2022-04-11 14:04:15 -0300

    Termino resumo Dart

--- EXERCICIOS_DART/resumoDart.dart
+++ EXERCICIOS_DART/resumoDart.dart
@@ -8,20 +8,25 @@ int soma(a, b) {
 class Produto {
   String nome = "";
   double preco = 0;
+
+//seria o construtor da classe, que fala como a mesma se inicia
+  Produto(String nome, double preco) {
+    this.nome = nome;
+    this.preco = preco;
+  }
+
+  //outro modelo de construtor, precisam ser feitos na ordem das varíaveis lançadas
+  // Produto(this.nome, this.preco);
 }
 
 // porta de entrada do sistema, tudo que será exutado precisa estar dentro do main
 void main(List<String> args) {
   print('O valor da soma é: ${soma(2, 5)}');
-  /*
-  em dart a concatenação é usado o ${aqui dentro é coloca o objeto a ser exibido}
-  antes/depois vem o denterminado comentario
-  */
+  /* em dart a concatenação é usado o ${aqui dentro é coloca o objeto a ser exibido}
+  antes/depois vem o denterminado comentario*/
 
 // inicio a Orientação a Objetos
-  var produto = new Produto();
-  produto.nome = "lapis";
-  produto.preco = 4.55;
+  var produto = new Produto("lapis", 4.55);
 
   print("O produto ${produto.nome} tem preco ${produto.preco}");
 }
