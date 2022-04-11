240990c37b4db1aec36ca340e3679b00be63f2c6
pedro-ddias <pedro.hdias@outlook.com>
2022-04-11 14:48:38 -0300

    Primeiro Widget

--- PROJETO_PERGUNTAS/lib/main.dart
+++ PROJETO_PERGUNTAS/lib/main.dart
@@ -1,67 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+// porta de entrada do programa para passarmos primeiro Widget
+void main(List<String> args) {
+  runApp(new PerguntasApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
+//faz com que possamos criar a Widget, por isso extendemos essa classe
+//a partir disso já importou o nosso componente
+class PerguntasApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Projeto Perguntas Mobile - Pedim',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Projeto Mobile Pedim'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return new MaterialApp(
+      home: Text('Ola Flutter'),
     );
   }
 }
