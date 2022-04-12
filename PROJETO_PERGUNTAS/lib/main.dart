3bcd0040d16c6e8479e357af415a2a521d8ff4c5
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-04-12 14:27:07 -0300

    Aula de Widgets

--- PROJETO_PERGUNTAS/lib/main.dart
+++ PROJETO_PERGUNTAS/lib/main.dart
@@ -3,15 +3,24 @@ import 'package:flutter/material.dart';
 
 // porta de entrada do programa para passarmos primeiro Widget
 void main(List<String> args) {
-  runApp(new PerguntasApp());
+  runApp(PerguntasApp());
 }
 
 //faz com que possamos criar a Widget, por isso extendemos essa classe
 //a partir disso já importou o nosso componente
 class PerguntasApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: Text('Ola Flutter'),
-    );
+    //cada componente tem um contexto
+    // Widget é a raiz na aplicação
+    return MaterialApp(
+        // estrutura da aplicação
+        home: Scaffold(
+      //appBar tem o um componente que chama-se AppBar
+      appBar: AppBar(
+        title: Text('Perguntas'),
+      ),
+      body: Text('Ola flutter, qual seu nome?'),
+    ));
   }
 }
