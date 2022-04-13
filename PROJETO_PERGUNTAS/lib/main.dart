d0ff8ce2f48e63d12c83ce34f910454d09e72eb9
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-04-13 09:38:14 -0300

    Aula Column e Conectar Função a botão

--- PROJETO_PERGUNTAS/lib/main.dart
+++ PROJETO_PERGUNTAS/lib/main.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, unnecessary_import, prefer_const_literals_to_create_immutables, unused_local_variable, unused_element, avoid_print
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,16 +14,43 @@ void main(List<String> args) {
 class PerguntasApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      "Qual a sua cor favorita?",
+      "Qual o seu animal favorito?"
+    ];
+
+    responder() {
+      print("Pergunta respondida!");
+    }
+
     //cada componente tem um contexto
     // Widget é a raiz na aplicação
     return MaterialApp(
         // estrutura da aplicação
         home: Scaffold(
-      //appBar tem o um componente que chama-se AppBar
-      appBar: AppBar(
-        title: Text('Perguntas'),
-      ),
-      body: Text('Ola flutter, qual seu nome?'),
-    ));
+            //appBar tem o um componente que chama-se AppBar
+            appBar: AppBar(
+              title: Text('Perguntas'),
+            ),
+            body: Column(children: [
+              //pergando a pergunta da posição 0 que criamos na lista no inicio do projeto
+              Text(perguntas[0]),
+              //o botao recebe um componente filho que é outro Widget
+              // ignore: deprecated_member_use
+              RaisedButton(
+                child: Text("Resposta 1"),
+                onPressed: responder(),
+              ),
+              // ignore: deprecated_member_use
+              RaisedButton(
+                child: Text("Resposta 2"),
+                onPressed: null,
+              ),
+              // ignore: deprecated_member_use
+              RaisedButton(
+                child: Text("Resposta 3"),
+                onPressed: null,
+              )
+            ])));
   }
 }
