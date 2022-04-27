dfdaf27810bb92d16ddf8bcdd02f12d30e7f4809
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-04-27 13:47:30 -0300

    Começo aula

---
+++ projeto_perguntas/lib/resposta.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: use_key_in_widget_constructors, deprecated_member_use, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    RaisedButton(
+      child: Text("Resposta 3"),
+      onPressed: null,
+    );
+  }
+}
