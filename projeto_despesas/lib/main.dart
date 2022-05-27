d39007c50fa05f296a1a79ef2e38a8cb92d1ba8d
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-05-27 11:30:42 -0300

    Estilo a Transações
    Personalizar cores e modos nas transações para a visualização

--- projeto_despesas/lib/main.dart
+++ projeto_despesas/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace, avoid_unnecessary_containers
 import 'package:despesas/models/transection.dart';
 import 'package:flutter/material.dart';
 
@@ -39,8 +39,47 @@ class MyHomePage extends StatelessWidget {
                 // ignore: prefer_const_literals_to_create_immutables
                 children: _transections.map((tr) {
                   return Card(
-                    child: Text(tr.title),
-                  );
+                      child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 3,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tr.value.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tr.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tr.date.toString(),
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 103, 103, 103),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ));
                 }).toList(),
               )
             ]));
