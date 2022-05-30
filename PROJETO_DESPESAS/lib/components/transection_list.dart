5e6654bd6d3ec0a7612b87ec7627a8681e0a2340
pedro-ddias <pedro.hdias@outlook.com>
2022-05-29 22:18:36 -0300

    Usando ListView

--- PROJETO_DESPESAS/lib/components/transection_list.dart
+++ PROJETO_DESPESAS/lib/components/transection_list.dart
@@ -12,53 +12,53 @@ class TransectionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: transections.map((tr) {
-            return Card(
-                child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 3,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
+      // para renderizar na tela a quantidade visivel
+      child: ListView.builder(
+        itemCount: transections.length,
+        itemBuilder: (ctx, index) {
+          final tr = transections[index];
+          return Card(
+              child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.purple,
+                    width: 3,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      tr.title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    tr.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      DateFormat('d/MM/y').format(tr.date),
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 103, 103, 103),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ));
-          }).toList(),
-        ),
+                  ),
+                  Text(
+                    DateFormat('d/MM/y').format(tr.date),
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 103, 103, 103),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ));
+        },
       ),
     );
   }
