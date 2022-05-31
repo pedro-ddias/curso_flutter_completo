8b4b937bcb0e85a53dc123bb35fa39bba8a4b742
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2022-05-31 12:26:29 -0300

    Atualização tema aplicação

--- projeto_despesas/lib/main.dart
+++ projeto_despesas/lib/main.dart
@@ -1,56 +1,91 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace, avoid_unnecessary_containers, deprecated_member_use, avoid_print, unused_import, unused_element
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, sized_box_for_whitespace
 
-import 'package:despesas/components/transection_form.dart';
 import 'package:flutter/material.dart';
-
-import 'package:intl/intl.dart';
-import 'components/transection_form.dart';
-import 'components/transection_list.dart';
-import 'models/transection.dart';
 import 'dart:math';
+import './components/transection_form.dart';
+import './components/transection_list.dart';
+import 'models/transection.dart';
 
-main() => runApp(DespesasApp());
+main() => runApp(ExpensesApp());
 
-class DespesasApp extends StatelessWidget {
+class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ThemeData tema = ThemeData();
+
     return MaterialApp(
       home: MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          // ignore: prefer_const_constructors
+          headline6: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        // ignore: prefer_const_constructors
+        appBarTheme: AppBarTheme(
+          // ignore: prefer_const_constructors
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transections = [
+  final _transactions = [
     transection(
-        date: DateTime.now(), id: 't1', title: 'Tenis Novo', value: 310.99),
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
     transection(
-        date: DateTime.now(), id: 't2', title: 'Conta Luz', value: 250.11),
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
   ];
 
-  _addTransection(String title, double value) {
-    final newTransection = transection(
-        date: DateTime.now(),
-        id: Random().nextDouble().toString(),
-        title: title,
-        value: value);
+  _addTransaction(String title, double value) {
+    final newTransaction = transection(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
 
     setState(() {
-      _transections.add(newTransection);
+      _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
-  _openTransectionFormModal(BuildContext context) {
+  _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
-        context: context,
-        builder: (_) {
-          return TransectionForm(_addTransection);
-        });
+      context: context,
+      builder: (_) {
+        return TransectionForm(_addTransaction);
+      },
+    );
   }
 
   @override
@@ -58,32 +93,32 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
-        // ignore: prefer_const_literals_to_create_immutables
-        actions: [
-          //botao de adcionar no AppBar
+        actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () => _openTransectionFormModal(context),
-          )
+            onPressed: () => _openTransactionFormModal(context),
+          ),
         ],
       ),
-      // para de rolagem para aparecer todos documentos na tela
       body: SingleChildScrollView(
         child: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  child: Text('Gráfico'),
-                ),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
               ),
-              TransectionList(_transections),
-            ]),
+            ),
+            TransectionList(_transactions),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => _openTransectionFormModal(context),
+        onPressed: () => _openTransactionFormModal(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
